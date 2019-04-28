c91ba147e504a47de2699c590af982a0988edb64
Patrick Wang <patrick.bohan.wang@gmail.com>
2019-04-28 08:16:47 -0700

    Added autonomous recording

--- dpad_controls-gpio.py
+++ dpad_controls-gpio.py
@@ -2,6 +2,7 @@ import time
 import os 
 import sys
 import json
+from multiprocessing import Process
 
 sys.path.insert(0, '/home/pi/.local/lib/python2.7/site-packages/')
 
@@ -9,46 +10,78 @@ import pigpio
 
 gpio = pigpio.pi()
 
-recording = false
-path = []
+auto = {
+    "recording": False,
+    "path": [],
+    "foot": 0.73469387748,
+    "rotation": 2.45
+}
+
+
+
+motor = {
+    "config": {
+        "drive": 0.5,
+        "lift": 1
+    },
+    "right": {
+        "name": "right",
+        "pins": [12, 16],
+        "value": 0
+    },
+    "left": {
+        "name": "left",
+        "pins": [21, 20],
+        "value": 0
+    },
+    "lift": {
+        "name": "lift",
+        "pins": [19, 26],
+        "value": 0
+    }
+}
 
 startTime = 0
-switchID = "31081732471476003969"
-count = 1
-time_count = 0
+switchID = "18851477588195929070"
 switch0 = False
 switch1 = False
 switch2 = False
-foot = 0.73469387748
-rotation = 2.45
-lift_motor_speed = 1
 stop_down=1
 stop_up=1
-drive_motor_speed = 0.5
-
 start_pos_up = 1
-
-lift=[19, 26]
-left_motor = [21, 20]
-right_motor = [12, 16]
-
-def setM(motor, speed):
-    if recording:
-        path.append({
-            "time": time.time() - startTime,
-            "motor": motor,
-            "speed": speed
-        })
-    if(speed >= 0):
-        gpio.write(motor[1], 0)
-        gpio.set_PWM_dutycycle(motor[0], 255 * speed)
-    else:
-        gpio.write(motor[0], 0)
-        gpio.set_PWM_dutycycle(motor[1], 255 * -speed)
+def setM(m, speed):
+    global startTime
+    global motor
+    global auto
+    if speed != motor[m["name"]]["value"]:
+        motor[m["name"]]["value"] = speed
+        if auto["recording"]:
+            auto["path"].append({
+                "time": time.time() - startTime,
+                "motor": {
+                    "name": motor[m["name"]]["name"],
+                    "pins": motor[m["name"]]["pins"],
+                    "value": speed
+                }
+            })
+            print(auto["path"])
+        if(speed >= 0):
+            gpio.write(m["pins"][1], 0)
+            gpio.set_PWM_dutycycle(m["pins"][0], 255 * speed)
+        else:
+            gpio.write(m["pins"][0], 0)
+            gpio.set_PWM_dutycycle(m["pins"][1], 255 * -speed)
     # setM(motor, speed)
+def resetMotors():
+    for m in motor:
+        if "pins" in m:
+            gpio.write(m["pins"][0], 0)
+            gpio.write(m["pins"][1], 0)
+
+resetMotors()
 def autonomous_setup():
     print("Autonomous mode has started!")
-    Robot.run(autonomous_actions())
+    Robot.run(autonomous_play)
 
 def autonomous_main():
     pass
@@ -57,10 +90,25 @@ def autonomous_main():
 async def autonomous_actions():
     #Tells robot to move forward:
     Robot.run(autonomous_move())
-    
+async def autonomous_play():
+    data = json.load(open('/home/pi/Autonomous/autonomous_data.json', 'r'))
+    lastTime = 0
+    startTime = time.time()
+    path = data[len(data) - 1]
+    while time.time() - startTime < path["time"]:
+        for i in range(0, len(path["path"]) - 1):
+            if(time.time() - startTime > path["path"][i]["time"] and time.time() - startTime < path["path"][i + 1]["time"]):
+                setM(path["path"][i]["motor"], path["path"][i]["motor"]["value"])
+                print("i: " + str(i) + ", " + path["path"][i]["motor"]["name"] + ": " + str(path["path"][i]["motor"]["value"]))
+        # if(time.time() - startTime > path["path"][len(path["path"]) - 1]["time"]):
+        #     setM(path["path"][len(path["path"])]["motor"], path["path"][len(path["path"])]["motor"]["value"])
+            
+async def auto_action(motor, time):
+    setM(motor, motor["value"])
+    await Actions.sleep(time)
     
 async def autonomous_move():
-    
+    pass
     
 
 def teleop_setup():
@@ -70,90 +118,107 @@ def teleop_main():
     global stop_up
     global stop_down
     global start_pos_up
-    global lift_motor_speed
-    global drive_motor_speed
+    global auto
+    global startTime
+    global motor
     dup = Gamepad.get_value("dpad_up")
     ddown = Gamepad.get_value("dpad_down")
     dleft = Gamepad.get_value("dpad_left")
     dright = Gamepad.get_value("dpad_right")
-    if Gamepad.get_value("button_y"):
-        if not recording:
-            recording = true
+    if Gamepad.get_value("button_start"):
+        if not auto["recording"]:
+            print("Recording")
+            auto["recording"] = True
             startTime = time.time()
-    if Gamepad.get_value("button_x"):
-        if recording:
-            recording = false
-            with open('autonomous_data.json') as autoData:
-                json.dump(json.load(autoData).append({
-                        "Title": "Untitled",
-                        "time": time.time() - startTime,
-                        "path": path
-                    }), autoData)
+        else:
+            print("Already recording")
+    if Gamepad.get_value("button_back"):
+        if auto["recording"]:
+            print("Logging Record")
+            auto["recording"] = False
+            data = json.load(open('/home/pi/Autonomous/autonomous_data.json', 'r'))
+            data.append({
+                "Title": "Untitled",
+                "time": time.time() - startTime,
+                "path": auto["path"]
+            })
+            
+            # print(json.dumps(data, indent=2))
+            json.dump(data, open('/home/pi/Autonomous/autonomous_data.json', 'w'), indent=2)
+        else:
+            print("Not recording")
+    if Gamepad.get_value("button_b"):
+        data = []
+        for i in range(0, 10):
+            data.append({
+                "test" + str(i): i
+            })
+        print(data)
     if Gamepad.get_value("r_bumper"):
-        drive_motor_speed = 1
+        motor["config"]["drive"] = 1
     if Gamepad.get_value("l_bumper"):
-        drive_motor_speed = 0.5
+        motor["config"]["drive"] = 0.5
     if dup:
         if dleft:
-            setM(left_motor, 0)
-            setM(right_motor, drive_motor_speed)
+            setM(motor["left"], 0)
+            setM(motor["right"], motor["config"]["drive"])
         elif dright:
-            setM(left_motor, drive_motor_speed)
-            setM(right_motor, 0)
+            setM(motor["left"], motor["config"]["drive"])
+            setM(motor["right"], 0)
         else:
-            setM(left_motor, drive_motor_speed)
-            setM(right_motor, drive_motor_speed)
+            setM(motor["left"], motor["config"]["drive"])
+            setM(motor["right"], motor["config"]["drive"])
     elif ddown:
         if dleft:
-            setM(left_motor, 0)
-            setM(right_motor, -drive_motor_speed)
+            setM(motor["left"], 0)
+            setM(motor["right"], -motor["config"]["drive"])
         elif dright:
-            setM(left_motor, -drive_motor_speed)
-            setM(right_motor, 0)
+            setM(motor["left"], -motor["config"]["drive"])
+            setM(motor["right"], 0)
         else:
-            setM(left_motor, -drive_motor_speed)
-            setM(right_motor, -drive_motor_speed)
+            setM(motor["left"], -motor["config"]["drive"])
+            setM(motor["right"], -motor["config"]["drive"])
     elif dright:
-        setM(right_motor, -drive_motor_speed)
-        setM(left_motor, drive_motor_speed)
+        setM(motor["right"], -motor["config"]["drive"])
+        setM(motor["left"], motor["config"]["drive"])
     elif dleft:
-        setM(right_motor, drive_motor_speed)
-        setM(left_motor, -drive_motor_speed)
+        setM(motor["right"], motor["config"]["drive"])
+        setM(motor["left"], -motor["config"]["drive"])
     else:
-        setM(right_motor, 0)
-        setM(left_motor, 0)
+        setM(motor["right"], 0)
+        setM(motor["left"], 0)
         
         
-    # setM(left_motor, (Gamepad.get_value("dpad_up") - Gamepad.get_value("dpad_down") + Gamepad.get_value("dpad_right") - Gamepad.get_value("dpad_left")) * drive_motor_speed)
-    # setM(right_motor, (Gamepad.get_value("dpad_up") - Gamepad.get_value("dpad_down") - Gamepad.get_value("dpad_right") + Gamepad.get_value("dpad_left")) * drive_motor_speed)
+    # setM(motor["left"], (Gamepad.get_value("dpad_up") - Gamepad.get_value("dpad_down") + Gamepad.get_value("dpad_right") - Gamepad.get_value("dpad_left")) * motor["config"]["drive"])
+    # setM(motor["right"], (Gamepad.get_value("dpad_up") - Gamepad.get_value("dpad_down") - Gamepad.get_value("dpad_right") + Gamepad.get_value("dpad_left")) * motor["config"]["drive"])
    
     if Gamepad.get_value("r_bumper"):
-        drive_motor_speed = 1
+        motor["config"]["drive"] = 1
     if Gamepad.get_value("l_bumper"):
-        drive_motor_speed = 0.5
+        motor["config"]["drive"] = 0.5
     if Gamepad.get_value("r_trigger") == 1:
         stop_down = 1  # clear stop value
         start_pos_up = 1
         if Robot.get_value(switchID,"switch0") and stop_up:
-            setM(lift, lift_motor_speed)# keep going UP
+            setM(motor["lift"], motor["config"]["lift"])# keep going UP
         else: # hit top limit switch, stop and reverse motor
-            setM(lift, 0.0)  # stop motor for now
+            setM(motor["lift"], 0.0)  # stop motor for now
     elif Gamepad.get_value("l_trigger") == 1 :
         start_pos_up = 1
         stop_up = 1  # clear stop value
         if Robot.get_value(switchID,"switch2") and stop_down:
-            setM(lift, -lift_motor_speed)# keeping going DOWN
+            setM(motor["lift"], -motor["config"]["lift"])# keeping going DOWN
         else: # hit bottom limit switch, stop and reverse motor
-            setM(lift, 0.0)  # stop motor for now
+            setM(motor["lift"], 0.0)  # stop motor for now
     elif Gamepad.get_value("button_a") == 1:
         if Robot.get_value(switchID,"switch1") and start_pos_up :
-            setM(lift, lift_motor_speed) # go up 
+            setM(motor["lift"], motor["config"]["lift"]) # go up 
         elif Robot.get_value(switchID,"switch1") == 0  :
-            setM(lift, 0.0)# stop
+            setM(motor["lift"], 0.0)# stop
             start_pos_up = 0
     else:
         start_pos_up = 1
-        setM(lift, 0.0)
+        setM(motor["lift"], 0.0)
 
 
 
